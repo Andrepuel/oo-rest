7710350973e9e732f922e6f28b040c39955c6719
André Puel <andrepuel@gmail.com>
2018-01-30 18:04:32 -0200

    UTF8 only websocket support

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,6 +1,11 @@
 import * as http from 'http';
 import * as restify from 'restify';
-import { OoServer } from './index';
+import * as websocket from 'websocket';
+import { IMessageHandler, OoServer } from './index';
+
+function tick(): Promise<void> {
+    return new Promise((ok) => setTimeout(ok, 10));
+}
 
 async function request(url: string): Promise<{response: http.IncomingMessage, body: any}> {
     const response = await new Promise<http.IncomingMessage>((ok) => {
@@ -17,6 +22,16 @@ async function request(url: string): Promise<{response: http.IncomingMessage, bo
     return {response, body: JSON.parse(body)};
 }
 
+async function connect(url: string): Promise<websocket.connection> {
+    const client = new websocket.client();
+    const conn = new Promise<websocket.connection>((ok, err) => {
+        client.once('connect', ok);
+        client.once('connectFailed', err);
+    });
+    client.connect(url);
+    return conn;
+}
+
 class TestSubPath extends OoServer {
     public async get_(): Promise<string> {
         return 'sub';
@@ -24,6 +39,8 @@ class TestSubPath extends OoServer {
 }
 
 class TestServer extends OoServer {
+    public handler: IMessageHandler;
+
     constructor() {
         super();
     }
@@ -38,6 +55,10 @@ class TestServer extends OoServer {
 
     public async get_sub() {
         return new TestSubPath();
+    }
+
+    public async ws_listen(): Promise<IMessageHandler> {
+        return this.handler;
     }
 }
 
@@ -60,6 +81,97 @@ describe('OoServer', () => {
     it('should access sub path', async () => {
         const result = await request('http://127.0.0.1:8080/sub');
         expect(result.body).to.be.equal('sub');
+    });
+
+    it('should receive and send messages on websocket', async () => {
+        const received = new Array<string>();
+        let closed = false;
+        let sendMsg: (msg: string) => Promise<void>;
+
+        rest.handler = {
+            closed: async () => {
+                closed = true;
+            },
+            msg: async (msg: string) => {
+                received.push(msg);
+            },
+            resultType: 'message-handler',
+            start: async (a1, a2) => {
+                sendMsg = a1;
+            },
+        };
+        const ws = await connect('http://127.0.0.1:8080/listen');
+        expect(received.length).to.be.equal(0);
+        ws.sendUTF('hello world');
+        await tick();
+        expect(received.length).to.be.equal(1);
+        expect(received[0]).to.be.equal('hello world');
+
+        let recvMsg = '';
+        ws.on('message', (data) => recvMsg = data.utf8Data);
+        await sendMsg('Hello world!');
+        await tick();
+        expect(recvMsg).to.be.equal('Hello world!');
+
+        expect(closed).to.be.equal(false);
+        ws.close();
+        await tick();
+        expect(closed).to.be.equal(true);
+    });
+
+    it('should be able to closed the socket', async () => {
+        let closed = false;
+        let clientClosed = false;
+        let close: (error?: number, description?: string) => Promise<void>;
+
+        rest.handler = {
+            closed: async () => {
+                closed = true;
+            },
+            msg: null,
+            resultType: 'message-handler',
+            start: async (a1, a2) => {
+                close = a2;
+            },
+        };
+
+        const ws = await connect('http://127.0.0.1:8080/listen');
+        ws.on('close', () => clientClosed = true);
+        await tick();
+        expect(clientClosed).to.be.equal(false);
+        await close();
+        await tick();
+        expect(clientClosed).to.be.equal(true);
+        expect(closed).to.be.equal(true);
+    });
+
+    it('should be able to close the websocket with error', async () => {
+        let closed = false;
+        let clientClosed = false;
+        let close: (error?: number, description?: string) => Promise<void>;
+
+        rest.handler = {
+            closed: async () => {
+                closed = true;
+            },
+            msg: null,
+            resultType: 'message-handler',
+            start: async (a1, a2) => {
+                close = a2;
+            },
+        };
+
+        const ws = await connect('http://127.0.0.1:8080/listen');
+        ws.on('close', (a1, a2) => {
+            expect(a1).to.be.equal(500);
+            expect(a2).to.be.equal('hello world!');
+            clientClosed = true;
+        });
+        await tick();
+        expect(clientClosed).to.be.equal(false);
+        await close(500, 'hello world!');
+        await tick();
+        expect(clientClosed).to.be.equal(true);
     });
 
     afterEach(async () => {
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+import * as assert from 'assert';
 import * as process from 'process';
 import * as restify from 'restify';
+import * as websocket from 'websocket';
 
 function nothrow(x: Promise<any>) {
     x.catch((e) => {
@@ -8,6 +10,20 @@ function nothrow(x: Promise<any>) {
         process.abort();
     });
 }
+function nothrowf(x: () => Promise<any>) {
+    nothrow(x());
+}
+
+export interface IMessageHandler {
+    resultType: 'message-handler';
+    start(sendMsg: (msg: string) => Promise<void>, close: (error: number, reason: string) => Promise<void>);
+    msg(msg: string): Promise<void>;
+    closed(reason: number, message: string): Promise<void>;
+}
+
+interface IWebsocketExtra {
+    handler: IMessageHandler;
+}
 
 export class OoServer {
     public setup(s: restify.Server): void {
@@ -15,31 +31,82 @@ export class OoServer {
         s.put('.*', this.handle.bind(this));
         s.post('.*', this.handle.bind(this));
         s.del('.*', this.handle.bind(this));
+        const ws = new websocket.server({httpServer: s});
+        ws.on('request', this.handleWs.bind(this));
     }
 
     public handle(req: restify.Request, res: restify.Response, next: restify.Next) {
         const url = req.url.split('/').filter((x) => x.length > 0);
-        nothrow(this.findRoute(url, req, res, next));
+        nothrowf(async () => {
+            try {
+                const r = await this.findRoute(url, req.method);
+                if (r === null) {
+                    return res.send(404);
+                }
+
+                res.send(r);
+            } catch (e) {
+                res.send(e);
+            } finally {
+                next();
+            }
+        });
     }
 
-    public async findRoute(url: string[], req: restify.Request, res: restify.Response, next: restify.Next) {
+    public async handleWs(req: websocket.request) {
+        const url = req.httpRequest.url.split('/').filter((x) => x.length > 0);
+        nothrowf(async () => {
+            let conn: websocket.connection&IWebsocketExtra = null;
+            try {
+                const r = await this.findRoute(url, 'ws') as IMessageHandler;
+                if (r === null) {
+                    return req.reject(404);
+                }
+
+                assert.equal(r.resultType, 'message-handler');
+                conn = req.accept() as (websocket.connection&IWebsocketExtra);
+                conn.handler = r;
+                conn.handler.start(async (msg) => {
+                    conn.sendUTF(msg);
+                },
+                async (error, reason) => {
+                    if (error) {
+                        conn.drop(error, reason);
+                    } else {
+                        conn.close();
+                    }
+                });
+                conn.on('message', (data) => nothrowf(async () => {
+                    await conn.handler.msg(data.utf8Data);
+                }));
+                conn.on('close', (a1, a2) => nothrowf(async () => {
+                    await conn.handler.closed(a1, a2);
+                }));
+            } catch (e) {
+                console.error(e);
+                if (conn !== null) {
+                    conn.drop(500, JSON.stringify(e));
+                } else {
+                    req.reject(500, JSON.stringify(e));
+                }
+            }
+        });
+    }
+
+    public async findRoute(url: string[], method: string) {
         if (url.length === 0) {
             url = [''];
         }
-        try {
-            const route = this[req.method.toLowerCase() + '_' + url[0]] || this['any_' + url[0]];
-            if (route) {
-                const x = await Promise.resolve().then(() => route(req, res));
-                if (x instanceof OoServer) {
-                    return x.findRoute(url.slice(1), req, res, next);
-                }
-                res.send(x);
-            } else {
-                res.send(404);
+
+        const route = this[method.toLowerCase() + '_' + url[0]] || this['any_' + url[0]];
+        if (route) {
+            const x = await Promise.resolve().then(() => route.apply(this));
+            if (x instanceof OoServer) {
+                return x.findRoute(url.slice(1), method);
             }
-            next();
-        } catch (e) {
-            next(e);
+            return x;
+        } else {
+            return null;
         }
     }
 }
